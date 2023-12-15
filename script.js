927242ef009e1db7ee576975f9d42e760116d8d6
Harsh Balwani <balwaniharsh1001@gmail.com>
2023-12-15 16:32:25 +0530

    Added some animations over text

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const scroll = new LocomotiveScroll({
 });
 
 //gsap for animations
+gsap.from(".nlink", {
+  stagger: 0.1,
+  y: 10,
+  duration: 1,
+});
+//shery js
+Shery.textAnimate("#headings h1", {
+  style: 2,
+  duration: 2,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0.1,
+});
+
+gsap.from(".anim2", {
+  y: 50,
+  stagger: 0.3,
+  opacity: 0,
+  ease: Expo,
+  duration: 1,
+});
